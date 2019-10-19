e8602e39f508982c4228cd50eedbc89f87eb8012
BvSiT <bvstraten1@gmail.com>
2019-10-20 00:27:43 +0200

    Task2

--- app/src/main/java/com/bvsit/simpleasynctask/SimpleAsyncTask.java
+++ app/src/main/java/com/bvsit/simpleasynctask/SimpleAsyncTask.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask<Void,Void,String[]> {
+public class SimpleAsyncTask extends AsyncTask<Void,Void,String> {
     WeakReference<TextView> mTextView;
 
     public SimpleAsyncTask(WeakReference<TextView> tv) {
@@ -14,10 +14,30 @@ public class SimpleAsyncTask extends AsyncTask<Void,Void,String[]> {
     }
 
     @Override
-    protected String[] doInBackground(Void... voids) {
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10
         Random r = new Random();
         int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running
         int s = n * 200;
-        return null;
+
+        // Sleep for the random amount of time
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        mTextView.get().setText(s);
     }
 }
